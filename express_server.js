3816b915c75b10ba0a62c10335f37f8406a7ef50
venetrius <gergelygjuhasz@gmail.com>
2019-05-07 20:43:53 +0000

    generate random string as a short url

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,6 +39,23 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", {templateVars});
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);
+  urlDatabase[generateRandomString()] = req.body.longURL;
+    // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+     let randomString = [];
+     for (let i = 0; i < 6; i++){
+      let randomNumber = Math.floor(Math.random() * characters.length);
+      randomString.push(characters[randomNumber]);
+     }
+     return randomString.join("");
+}
